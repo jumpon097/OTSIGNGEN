652bf4b7aecbd4bfeceacd1a9d617812d495a00b
jumpon097 <38902090+jumpon097@users.noreply.github.com>
2026-06-30 19:54:25 +0700

    Delete app.js

--- app.js
+++
@@ -1,2 +0,0 @@
-<!doctype html>
-<html lang="th"><head><meta charset="utf-8"><meta http-equiv="refresh" content="0; url=./"><title>กำลังกลับหน้าแรก</title></head><body>กำลังกลับหน้าแรก…</body></html>
